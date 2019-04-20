23404864e40557b0b9af950f4a0827dc4e539c5f
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2019-04-20 09:29:28 +0200

    Fixed RAZER_ATROX_BUTTON_STATE stuct. Usage of bitfields was breaking button press detection. Removed unused PIPE_ID struct

--- RazorAtroxWinUSBTest/main.h
+++ RazorAtroxWinUSBTest/main.h
@@ -1,11 +1,5 @@
 #pragma once
 
-struct PIPE_ID
-{
-	UCHAR  PipeInId;
-	UCHAR  PipeOutId;
-}; 
-
 UCHAR RAZER_ATROX_INIT[] = {
 	0x05, 0x20, 0x08, 0x01, 0x05
 };
@@ -18,21 +12,21 @@ struct RAZER_ATROX_DATA_PACKET
 
 struct RAZER_ATROX_BUTTON_STATE
 {
-    BOOL buttonX : 1;
-    BOOL buttonY : 1;
-    BOOL buttonA : 1;
-    BOOL buttonB : 1;
-    BOOL rightButton : 1;
-    BOOL leftButton : 1;
-    BOOL rightTrigger : 1;
-    BOOL leftTrigger : 1;
-    BOOL buttonMenu : 1;
-    BOOL buttonView : 1;
-    BOOL buttonGuide : 1;
-    BOOL stickUp : 1;
-    BOOL stickLeft : 1;
-    BOOL stickDown : 1;
-    BOOL stickRight : 1;
+    BOOL buttonX;
+    BOOL buttonY;
+    BOOL buttonA;
+    BOOL buttonB;
+    BOOL rightButton;
+    BOOL leftButton;
+    BOOL rightTrigger;
+    BOOL leftTrigger;
+    BOOL buttonMenu;
+    BOOL buttonView;
+    BOOL buttonGuide;
+    BOOL stickUp;
+    BOOL stickLeft;
+    BOOL stickDown;
+    BOOL stickRight;
 };
 
 enum RAZER_ATROX_PACKET_TYPES
